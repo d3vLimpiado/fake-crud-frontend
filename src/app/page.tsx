77b03caf5908fac83e7025db788e63b4706c662a
joshua limpiado <jianlimpiado@gmail.com>
2024-09-10 00:23:34 +0800

    feat: initial development, nextjs, shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-svh grid place-items-center">
+      <Button>EYYY FIRST BUTTON!</Button>
+    </div>
+  );
+}
