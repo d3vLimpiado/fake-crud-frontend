6c7712b74c96767c29e696a0b6aec01f11cb4fc8
jin <jianlimpiado@gmail.com>
2024-09-19 00:00:23 +0800

    feat: update inital ui/tables/buttons/badge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,45 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import Typography from '@/components/ui/typography';
 
 const {H1} = Typography
 
 export default function Home() {
   return (
-    <div className="h-svh flex flex-col justify-center items-center">
+    <div className="container h-svh flex flex-col justify-start items-start">
       <H1>FAKE CRUD</H1>
-      <Button>EYYY FIRST BUTTON!</Button>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead className="text-right">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">Limpiado, Joshua Barcial</TableCell>
+            <TableCell>
+              <Badge variant="success">Enrolled</Badge>
+            </TableCell>
+            <TableCell className="flex gap-2 justify-end">
+              <Button>EDIT</Button>
+              <Button>DELETE</Button>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Limpiado, Joshua Barcial</TableCell>
+            <TableCell>
+              <Badge variant="destructive">Not Enrolled</Badge>
+            </TableCell>
+            <TableCell className="flex gap-2 justify-end">
+              <Button>EDIT</Button>
+              <Button>DELETE</Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -47,6 +47,10 @@ const config: Config = {
   				DEFAULT: 'var(--destructive)',
   				foreground: 'var(--destructive-foreground)'
   			},
+				success: {
+  				DEFAULT: 'var(--success)',
+  				foreground: 'var(--success-foreground)'
+  			},
   			border: 'var(--border)',
   			input: 'var(--input)',
   			ring: 'var(--ring)',
